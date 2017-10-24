1568c122d6fe58c0ddf24bc1d761933a0878df93
Gabriel Parelli Francischini <gpfrancischini@gmail.com>
2017-10-24 18:59:38 -0200

    Make component a stateless component

--- src/pages/MyReadsPage.js
+++ src/pages/MyReadsPage.js
@@ -4,35 +4,34 @@ import { Link } from 'react-router-dom'
 import Spinner from 'react-spinkit';
 import PropTypes from 'prop-types';
 
-class MyReadsPage extends React.Component {
-    render() {
-        return (
-            <div className="list-books">
-                <div className="list-books-title">
-                    <h1>I Love Books</h1>
-                </div>
+const MyReadsPage = ({ shelves, books, isLoading, onBookShelfChange }) => (
 
-                {this.props.isLoading ?
-                    (
-                        <div className="list-books-loading">
-                            <Spinner fadeIn="none" name="ball-pulse-sync" color="#60ac5d" />
-                        </div>
-                    ) :
-                    (
-                        <BookShelfList
-                            shelves={this.props.shelves}
-                            books={this.props.books}
-                            onBookShelfChange={this.props.onBookShelfChange} />
-                    )
-                }
+    <div className="list-books">
+        <div className="list-books-title">
+            <h1>I Love Books</h1>
+        </div>
 
-                <div className="open-search">
-                    <Link to="/search">Add a book</Link>
+        {isLoading ?
+            (
+                <div className="list-books-loading">
+                    <Spinner fadeIn="none" name="ball-pulse-sync" color="#60ac5d" />
                 </div>
-            </div>
-        )
-    }
-}
+            ) :
+            (
+                <BookShelfList
+                    shelves={shelves}
+                    books={books}
+                    onBookShelfChange={onBookShelfChange} />
+            )
+        }
+
+        <div className="open-search">
+            <Link to="/search">Add a book</Link>
+        </div>
+    </div>
+)
+
+
 
 MyReadsPage.propTypes = {
 	/**
